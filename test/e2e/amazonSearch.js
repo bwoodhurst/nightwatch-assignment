4f4e23f93e332e9e2dc96fa0168d3c5312deb3c1
Brett Woodhurst <bwoodhurst@catalystitservices.com>
2017-11-01 22:26:34 -0700

    wip

--- test/e2e/amazonSearch.js
+++ test/e2e/amazonSearch.js
@@ -1,23 +1,39 @@
 var config = require('../../nightwatch.conf.BASIC.js');
 
 module.exports = {
-  'Should navigate to the first pair of most expensive shoes from a search': function(browser) {
-    browser
-        .url('https://www.amazon.com/')
-        .waitForElementVisible('input[id="twotabsearchtextbox"]')
+	'Should navigate to the first pair of the most expensive shoes from a search': function(browser) {
+		browser
+		.url('https://www.amazon.com/')
+		.waitForElementVisible('input[id="twotabsearchtextbox"]')
 		.setValue('input[id="twotabsearchtextbox"]', 'Nike Shoes').keys(browser.Keys.ENTER);
 		
+		var prices = [];
+		
 		var shoesOnSearchedPage = browser.elements('css selector', 'span.sx-price-whole', function(result) {
-			for (var i = 0; i < result.value.length; i++) {
-				this.elementIdValue(result.value[i].ELEMENT, 'abc');
+			for(var i = 0; i < result.value.length; i++) {
+				this.elementIdAttribute(result.value[i].ELEMENT, 'innerHTML', function(result) {
+					console.log('Price#', result.value);
+					prices.push(Number(result.value))
+				});
 			}
-		})
+	})
+	
+	browser.perform(function(done) {
+		console.log('prices', prices);
 		
-		var mostExpensiveShoesOnSearchedPage = Math.max(shoesOnSearchedPage)
-		browser.click(mostExpensiveShoesOnSearchedPage)
+		prices.sort(function(a, b) {
+			return a - b;
+		});
 		
-		//check mostExpensiveShoesOnSearchedPage against element on product page id="priceblock_ourprice".getText();
-        .end();
-  }
-  
+		console.log('prices', prices);
+		
+		done();
+	})
+	
+	var mostExpensiveShoesOnSearchedPage = Math.max(shoesOnSearchedPage)
+	browser.click(mostExpensiveShoesOnSearchedPage)
+	
+       .end();
+ }
+ 
 };
